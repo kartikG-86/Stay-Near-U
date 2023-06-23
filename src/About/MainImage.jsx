06d166ca29257237f1ffdf0153f50e8d059a691f
Kartik <kartikgoyal0852@gmail.com>
2023-06-23 22:26:10 +0530

    All Code

--- src/About/MainImage.jsx
+++ src/About/MainImage.jsx
@@ -6,6 +6,7 @@ const MainImage = () => {
       <div
         style={{
           height: "100vh",
+          width: "100%",
           backgroundImage: `url("https://news.airbnb.com/wp-content/uploads/sites/4/2022/09/Newsroom-About.jpg?w=2048")`,
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
