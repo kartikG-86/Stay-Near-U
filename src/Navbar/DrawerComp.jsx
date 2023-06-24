f6104e0586c33a5e2d8f62d588052c24a7796175
Kartik <kartikgoyal0852@gmail.com>
2023-06-24 10:23:27 +0530

    Hamburger Icon Done

--- src/Navbar/DrawerComp.jsx
+++ src/Navbar/DrawerComp.jsx
@@ -6,20 +6,52 @@ import {
   ListItemIcon,
   ListItemButton,
   IconButton,
+  Typography,
 } from "@mui/material";
 import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
+import { Link } from "react-router-dom";
 
 const Pages = ["Home", "About Us", "Contact Us", "Policy", "Login", "Sign up"];
+const routeChange = [
+  "/",
+  "/aboutUs",
+  "/contactUs",
+  "/policy",
+  "/login",
+  "/signUp",
+];
 let DrawerComp = () => {
   const [openDrawer, setOpenDrawer] = useState(false);
   return (
     <React.Fragment>
       <Drawer open={openDrawer} onClose={() => setOpenDrawer(false)}>
-        <List>
+        <List
+          style={{
+            backgroundColor: "black",
+            width: "10rem",
+            height: "100vh",
+            margin: "0rem",
+          }}
+        >
           {Pages.map((page, index) => (
             <ListItemButton onClick={() => setOpenDrawer(!openDrawer)}>
               <ListItemIcon>
-                <ListItemText>{page}</ListItemText>
+                <Link
+                  to={routeChange[index]}
+                  style={{ textDecoration: "none" }}
+                >
+                  <ListItemText
+                    style={{
+                      color: "white",
+                      borderBottom: "1px solid white",
+                      paddingBottom: "0.5rem",
+
+                      width: "8rem",
+                    }}
+                  >
+                    <Typography align="center">{page}</Typography>
+                  </ListItemText>
+                </Link>
               </ListItemIcon>
             </ListItemButton>
           ))}
